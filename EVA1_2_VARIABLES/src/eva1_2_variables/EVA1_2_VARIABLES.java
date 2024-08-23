28d04136a4712a13509c907ad4e23c79e8a17272
SMB-TEC2-14 <L24550835@chihuahua2.tecnm.mx>
2024-08-23 17:50:01 -0600

    Add files via upload

---
+++ EVA1_2_VARIABLES/src/eva1_2_variables/EVA1_2_VARIABLES.java
@@ -0,0 +1,54 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_2_variables;
+
+/**
+ *
+ * @author santy
+ */
+public class EVA1_2_VARIABLES {
+
+    public static void main(String[] args) {//Trabajo en clase sobre lo que se puede imprimir con (println)
+                                            //Tipos de Datos
+        System.out.println("Hello World!"); // El Hello World es una cadena de texto
+        System.out.println(100);            //El 100 es un numéro entero
+        System.out.println(3.1416);         //El 3.1416 es un numéro flotante
+        System.out.println("c");            //La c es un caracter
+        System.out.println(true);           //Es verdadero y falso (booleanos)
+        System.out.println(false);          //Es verdadero y falso (booleanos)
+        System.out.println(
+                false
+        )
+                   ;//EL ; es el fin de la instrucción (si después de cualquier caracter no hay nada Java va a ignorar espacios en blanco, independientemente de como este escrito el código
+        
+                //Enteros
+                //int
+                //long
+                //short
+                
+                //Numeros con parte decimal
+                //float
+                //double
+                
+                //Cadenas de texto
+                //string
+                
+                //Caracteres
+                //char
+                //Valores lógicos
+                //boolean
+                
+                //Declaración de variables
+                //Sintaxis para declarar la variable
+                //Tipo de dato nombre de la variable;
+                string nombre;
+                string apellido;
+                string CURP;
+                float promedio;
+                
+                
+                
+    }
+}
