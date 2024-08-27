8bf53b2ffa32026fe9aa7e3b49209f6bced48ce6
SMB-TEC2-14 <L24550835@chihuahua2.tecnm.mx>
2024-08-26 18:52:38 -0600

    Add files via upload

---
+++ EVA1_3_VARIABLES/src/eva1_3_variables/EVA1_3_VARIABLES.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_3_variables;
+
+/**
+ *
+ * @author santy
+ */
+public class EVA1_3_VARIABLES {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+                        //Asignación
+                String marca;
+                String modelo;
+                int year;
+                double precio;
+                
+                marca = "Nissan";
+                modelo = "GTR";
+                year = 2008;
+                precio = 1000000.1;
+                
+                System.out.println(marca);
+                System.out.println(modelo);
+                System.out.println(year);
+                System.out.println(precio);
+    }
+    
+    
+}
